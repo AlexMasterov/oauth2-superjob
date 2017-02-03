cbc23b49da6614021e4bc83c6df6e69971b05b1c
Alex Masterov <alex.masterow@gmail.com>
2017-02-03 21:16:55 +0300

    Upgrade PHPUnit version to 6.0

--- tests/SuperJobTest.php
+++ tests/SuperJobTest.php
@@ -7,7 +7,7 @@ use AlexMasterov\OAuth2\Client\Provider\SuperJob;
 use Eloquent\Phony\Phpunit\Phony;
 use GuzzleHttp\ClientInterface;
 use League\OAuth2\Client\Token\AccessToken;
-use PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 
 class SuperJobTest extends TestCase
@@ -15,7 +15,7 @@ class SuperJobTest extends TestCase
     /**
      * @var SuperJob
      */
-    protected $provider;
+    private $provider;
 
     protected function setUp()
     {
@@ -31,11 +31,28 @@ class SuperJobTest extends TestCase
         parent::tearDown();
     }
 
+    protected function mockResponse($body)
+    {
+        $response = Phony::mock(ResponseInterface::class);
+        $response->getHeader->with('content-type')->returns('application/json');
+        $response->getBody->returns(json_encode($body));
+
+        return $response;
+    }
+
+    protected function mockClient(ResponseInterface $response)
+    {
+        $client = Phony::mock(ClientInterface::class);
+        $client->send->returns($response);
+
+        return $client;
+    }
+
     public function testAuthorizationUrl()
     {
         // Run
         $url = $this->provider->getAuthorizationUrl();
-        $path = parse_url($url, PHP_URL_PATH);
+        $path = \parse_url($url, PHP_URL_PATH);
 
         // Verify
         $this->assertSame('/authorize/', $path);
@@ -47,7 +64,7 @@ class SuperJobTest extends TestCase
 
         // Run
         $url = $this->provider->getBaseAccessTokenUrl($params);
-        $path = parse_url($url, PHP_URL_PATH);
+        $path = \parse_url($url, PHP_URL_PATH);
 
         // Verify
         $this->assertSame('/2.0/oauth2/access_token/', $path);
@@ -68,37 +85,31 @@ class SuperJobTest extends TestCase
 
     public function testGetAccessToken()
     {
-        $rawResponse = [
+        $body = [
             'access_token'  => 'mock_access_token',
             'refresh_token' => 'mock_refresh_token',
             'ttl'           => 1394748311,
-            'expires_in'    => time() * 3600,
+            'expires_in'    => \time() * 3600,
             'token_type'    => 'bearer',
         ];
 
-        $response = Phony::mock(ResponseInterface::class);
-        $response->getHeader->with('content-type')->returns('application/json');
-        $response->getBody->returns(json_encode($rawResponse));
-
-        $client = Phony::mock(ClientInterface::class);
-        $client->send->returns($response->get());
+        $response = $this->mockResponse($body);
+        $client = $this->mockClient($response->get());
 
         // Run
         $this->provider->setHttpClient($client->get());
-        $token = $this->provider->getAccessToken('authorization_code', [
-            'code' => 'mock_authorization_code',
-        ]);
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
 
         // Verify
-        $this->assertEquals($rawResponse['access_token'], $token->getToken());
-        $this->assertEquals($rawResponse['refresh_token'], $token->getRefreshToken());
-        $this->assertGreaterThanOrEqual($rawResponse['expires_in'], $token->getExpires());
         $this->assertNull($token->getResourceOwnerId());
+        $this->assertEquals($body['access_token'], $token->getToken());
+        $this->assertEquals($body['refresh_token'], $token->getRefreshToken());
+        $this->assertGreaterThanOrEqual($body['expires_in'], $token->getExpires());
     }
 
     public function testUserProperty()
     {
-        $rawProperty = [
+        $body = [
             'id'           => 123,
             'name'         => 'name',
             'phone_number' => '+77777777777',
@@ -106,66 +117,50 @@ class SuperJobTest extends TestCase
             'date_reg'     => 1152323382,
         ];
 
-        $response = Phony::mock(ResponseInterface::class);
-        $response->getHeader->with('content-type')->returns('application/json');
-        $response->getBody->returns(json_encode($rawProperty));
-
-        $client = Phony::mock(ClientInterface::class);
-        $client->send->returns($response->get());
-
-        $token = new AccessToken([
+        $tokenOptions = [
             'access_token' => 'mock_access_token',
-            'expires_in' => 3600,
-        ]);
+            'expires_in'   => 3600,
+        ];
+
+        $token = new AccessToken($tokenOptions);
+        $response = $this->mockResponse($body);
+        $client = $this->mockClient($response->get());
 
         // Run
         $this->provider->setHttpClient($client->get());
         $user = $this->provider->getResourceOwner($token);
 
         // Verify
-        $this->assertEquals($rawProperty['id'], $user->getId());
-        $this->assertEquals($rawProperty['name'], $user->getName());
-        $this->assertEquals($rawProperty['phone_number'], $user->getPhoneNumber());
-        $this->assertEquals($rawProperty['email'], $user->getEmail());
-
+        $this->assertEquals($body['id'], $user->getId());
+        $this->assertEquals($body['name'], $user->getName());
+        $this->assertEquals($body['phone_number'], $user->getPhoneNumber());
+        $this->assertEquals($body['email'], $user->getEmail());
         $this->assertArrayHasKey('date_reg', $user->toArray());
     }
 
     public function testErrorResponses()
     {
-        $error = [
-            'code'    => 401,
-            'message' => 'Error message',
-            'error'   => 'Foo error',
+        $code = 401;
+        $body = [
+            'error' => [
+                'code'    => $code,
+                'error'   => 'Foo error',
+                'message' => 'Error message',
+            ],
         ];
 
-        $message = $error['error'].': '. $error['message'];
+        $response = $this->mockResponse($body);
+        $response->getStatusCode->returns($code);
+        $client = $this->mockClient($response->get());
 
-        $response = Phony::mock(ResponseInterface::class);
-        $response->getStatusCode->returns(401);
-        $response->getHeader->with('content-type')->returns('application/json');
-        $response->getBody->returns(json_encode(compact('error')));
+        $this->expectException(SuperJobException::class);
+        $this->expectExceptionCode($code);
 
-        $client = Phony::mock(ClientInterface::class);
-        $client->send->returns($response->get());
+        unset($body['error']['code']);
+        $this->expectExceptionMessage(implode(': ', $body['error']));
 
         // Run
         $this->provider->setHttpClient($client->get());
-
-        $errorMessage = '';
-        $errorCode = 0;
-
-        try {
-            $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
-        } catch (SuperJobException $e) {
-            $errorMessage = $e->getMessage();
-            $errorCode = $e->getCode();
-            $errorBody = $e->getResponseBody();
-        }
-
-        // Verify
-        $this->assertEquals($message, $errorMessage);
-        $this->assertEquals(401, $errorCode);
-        $this->assertEquals(json_encode(compact('error')), $errorBody);
+        $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
     }
 }
